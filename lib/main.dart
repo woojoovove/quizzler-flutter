e1fa86285a1bed7cc4064e103075736cc2b3e3bf
woojoovove <ujinjeong55@gmail.com>
2024-12-29 12:04:28 +0900

    [add] declare widget list

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,17 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Widget> scoreKeeper = [
+    Icon(
+      Icons.check,
+      color: Colors.green,
+    ),
+    Icon(
+      Icons.close,
+      color: Colors.red,
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Column(
